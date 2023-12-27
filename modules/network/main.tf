e4d8330d2a5aead37f89dd42e0f51ad1230fb0ff
Venugopal Krishnappa <venguopal.krishnappa@publicissapient.com>
2023-12-27 14:55:18 +0530

    changes location

--- modules/network/main.tf
+++ modules/network/main.tf
@@ -1,25 +1,25 @@
 resource "azurerm_resource_group" "rg" {
   name     = var.rg_name
-  location = var.resource_group_location
+  location = var.rg_location
   tags     = var.tags
 }
 
 resource "azurerm_virtual_network" "this" {
   name                = var.name
   address_space       = [var.address_space]
-  location            = azurerm_resource_group.this.location
-  resource_group_name = azurerm_resource_group.this.name
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
 }
 resource "azurerm_subnet" "public" {
   name                 = "${var.name}-public-subnet"
-  resource_group_name  = azurerm_resource_group.this.name
+  resource_group_name  = azurerm_resource_group.rg.name
   virtual_network_name = azurerm_virtual_network.this.name
   address_prefixes     = [cidrsubnet(var.address_space, 8, 1)]
 }
 
 resource "azurerm_subnet" "private" {
   name                 = "${var.name}-private-subnet"
-  resource_group_name  = azurerm_resource_group.this.name
+  resource_group_name  = azurerm_resource_group.rg.name
   virtual_network_name = azurerm_virtual_network.this.name
   address_prefixes     = [cidrsubnet(var.address_space, 8, 10)]
 }
