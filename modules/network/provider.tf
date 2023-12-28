ea64a304f065ebe96ba76fd5a35d186da46bf29e
Venugopal Krishnappa <venguopal.krishnappa@publicissapient.com>
2023-12-28 08:45:44 +0530

    provider commented

--- modules/network/provider.tf
+++ modules/network/provider.tf
@@ -1,17 +1,17 @@
-terraform {
-  required_version = ">= 1.2.6"
-  required_providers {
-    azurerm = {
-      source  = "hashicorp/azurerm"
-      version = ">= 3.24.0"
-    }
-    random = {
-      source  = "hashicorp/random"
-      version = ">= 3.0.0"
-    }
-  }
-}
+# terraform {
+#   required_version = ">= 1.2.6"
+#   required_providers {
+#     azurerm = {
+#       source  = "hashicorp/azurerm"
+#       version = ">= 3.24.0"
+#     }
+#     random = {
+#       source  = "hashicorp/random"
+#       version = ">= 3.0.0"
+#     }
+#   }
+# }
 
-provider "azurerm" {
-  features {}
-}
+# provider "azurerm" {
+#   features {}
+# }
